ec13d106da4ee6ac35f825581b6ff5efb1da20f8
pikuskij <joao.pikuski.barbosa@escola.pr.gov.br>
2026-06-25 09:57:29 -0300

    criei botao de curtida

---
+++ script.js
@@ -0,0 +1,7 @@
+const curtidas = document.getElementById("curtidas") /*pega o botão de curtidas pelo id*/
+curtidas.addEventListener("click", aumentarCurtidas)/*quando clicar chama as curtidas*/
+//função de aumentar curtidas
+function aumentarCurtidas{
+    let quantidade = document.querySelector("span")
+    quantidade.textContent++
+}
